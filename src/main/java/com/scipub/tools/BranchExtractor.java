8b867d37b1162c64b85a92fe239f11f7dee77fee
bozhanov <bozhidar.bozhanov@tomtom.com>
2015-02-14 23:49:30 +0200

    science taxonomy complete

--- src/main/java/com/scipub/tools/BranchExtractor.java
+++ src/main/java/com/scipub/tools/BranchExtractor.java
@@ -17,11 +17,10 @@ public class BranchExtractor {
         InputStream in = BranchExtractor.class.getResourceAsStream("/sql/sciences.txt");
         List<String> lines = CharStreams.readLines(new InputStreamReader(in, Charsets.UTF_8));
         
-        Pattern p = Pattern.compile("\\[\\[[^\\[\\]]+\\]\\]");
+        Pattern p = Pattern.compile("\\[\\[([^\\[\\]]+)\\]\\]");
         Stack<Branch> stack = new Stack<Branch>();
         List<Branch> branches = new ArrayList<>();
         int id = 1;
-        int currentLevel = 0;
         for (String line : lines) {
             int level;
             if (line.startsWith("*")) {
@@ -33,31 +32,29 @@ public class BranchExtractor {
             
             Matcher m = p.matcher(line);
             if (m.find()) {
-                String name = m.group();
+                String name = m.group(1);
                 Branch branch = new Branch();
                 branch.setName(name);
                 branch.setId(id);
 
+                while (stack.size() > level) {
+                    stack.pop();
+                }
                 if (!stack.isEmpty()) {
                     branch.setParentId(stack.peek().getId());
                 }
-                
-                if (level < stack.size()) {
-                    stack.pop();
-                } else if (level > stack.size()) {
-                    stack.push(branch);
-                }
-                
+                stack.push(branch);
                 branches.add(branch);
                 id++;
                 
                 
             } else {
-                System.out.println("!!!!!!!!!!!!!!!!!!!!! " + line);
+                throw new IllegalStateException("Input file is not well formed at: " + line);
             }
         }
         for (Branch branch : branches) {
-            System.out.println(branch);
+            System.out.println("INSERT INTO `branches` VALUES (" + branch.getId() + ",\""
+                    + branch.name.replace("\"", "'") + "\"," + branch.getParentId() + ");");
         }
     }
     
